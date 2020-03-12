323141ab6192dafc1b68e1a43ff1658f14040cad
Patric Pereira <patric.pereirafranca@gmail.com>
2020-03-12 13:54:27 -0300

    refactoring store publication

--- src/controllers/PublicationController.js
+++ src/controllers/PublicationController.js
@@ -1,27 +1,48 @@
+const Yup = require("yup");
 const Publication = require("../models/Publications");
 
 class PublicationController {
   async store(req, res) {
+    const schema = Yup.object().shape({
+      title: Yup.string().required(),
+      author: Yup.string().required(),
+      description: Yup.string().required(),
+      date: Yup.string().required(),
+      category: Yup.string().required()
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: "Validation fails" });
+    }
+
     const publication = await Publication.create(req.body);
 
     return res.status(201).json(publication);
   }
 
   async index(req, res) {
-    const publications = await Publication.find({ deleted_at: null });
+    try {
+      const publications = await Publication.find({ deleted_at: null });
 
-    return res.status(200).json(publications);
+      return res.status(200).json(publications);
+    } catch (error) {
+      return res.status(400).json(error);
+    }
   }
 
   async delete(req, res) {
-    const publication = await Publication.findAndModify(
-      {
-        _id: req.params.id
-      },
-      { deleted_at: Date.now() }
-    );
+    try {
+      const publication = await Publication.findAndModify(
+        {
+          _id: req.params.id
+        },
+        { deleted_at: Date.now() }
+      );
 
-    return res.status(200).json();
+      return res.status(200).json();
+    } catch (error) {
+      return res.status(400).json(error);
+    }
   }
 }
 
